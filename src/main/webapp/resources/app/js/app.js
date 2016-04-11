7dcb64d7e376971794b9bf33c1587434894023c9
gilmario-kpslow <gilmariosoftware@gmail.com>
2016-04-11 09:32:46 -0300

    Inicio

---
+++ src/main/webapp/resources/app/js/app.js
@@ -0,0 +1,25 @@
+function mascaraData(input) {
+    $(input).attr("maxlength", '10');
+    $(input).mask("99/99/9999", {placeholder: "dd/mm/yyyy"});
+}
+
+function mascaraCurrency(input) {
+    $(input).maskMoney({
+        thousands: '.', decimal: ',', allowZero: true, prefix: 'R$ '
+    });
+}
+
+function mostrarMenu(id, event) {
+    var X = event.clientX;
+    var Y = event.clientY;
+    var menu = document.getElementById(id);
+    menu.style.top = Y.toString() + "px";
+    menu.style.left = X.toString() + "px";
+    menu.style.visibility = "visible";
+    var lis = document.querySelectorAll("#" + id + " > ul > li");
+    for (var i = 0; i < lis.length; i++) {
+        lis.item(i).addEventListener("click", function () {
+            menu.style.visibility = "hidden";
+        });
+    }
+}
