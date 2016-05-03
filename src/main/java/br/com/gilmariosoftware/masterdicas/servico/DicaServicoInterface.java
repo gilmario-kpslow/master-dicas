cbc9a8e98190a424889576cd0de99799fb42eb31
gilmario-kpslow <gilmariosoftware@gmail.com>
2016-05-03 16:38:25 -0300

    ok

---
+++ src/main/java/br/com/gilmariosoftware/masterdicas/servico/DicaServicoInterface.java
@@ -0,0 +1,15 @@
+package br.com.gilmariosoftware.masterdicas.servico;
+
+import br.com.gilmariosoftware.masterdicas.dominio.Tag;
+import java.util.List;
+
+/**
+ *
+ * @author gilmario
+ */
+public interface TagServicoInterface {
+
+    public List<Tag> listar();
+
+    public String testando();
+}
