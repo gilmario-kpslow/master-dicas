1b9b767e61c5b59ce53877f12a28e8924f819e22
gilmario-kpslow <gilmariosoftware@gmail.com>
2017-03-09 13:23:53 -0300

    Criação do Gerar Senha e do WebService

--- MasterDicasWeb/src/main/webapp/resources/app/js/app.js
+++ MasterDicasWeb/src/main/webapp/resources/app/js/app.js
@@ -25,8 +25,26 @@ function mostrarMenu(id, event) {
 }
 
 
+
 $(document).ready(function () {
     $('.modal').modal();
     $(".button-collapse").sideNav();
     $('.tooltipped').tooltip({delay: 150});
+
+    jsf.ajax.addOnEvent(function (evento) {
+        if (evento.status === 'complete') {
+            console.log("COMPLETE");
+        }
+        if (evento.status === 'success') {
+            console.log(evento.responseText);
+            var j = $(evento.responseXML);
+
+            console.log("SUCCESS");
+            //Materialize.updateTextFields();
+            $(".ajax-indicador").addClass("ajax-indicador-sucesso");
+        }
+        if (evento.status === 'error') {
+            console.log("ERRO");
+        }
+    });
 });
